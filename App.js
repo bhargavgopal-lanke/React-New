1086a2970e8647a1fcbb80d1d39a3be3f7026b48
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-11-18 22:50:14 +0530

    code cleanup

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import logoimage from "./public/logo-image.webp";
-
-const HeaderComponent = () => {
-  return (
-    <div className="header">
-      <div className="logo">
-        <img src={logoimage} alt="" title="logo" />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+import Header from "./components/Header";
 
 const AppLayout = () => {
   return (
     <div className="app">
-      <HeaderComponent />
+      <Header />
     </div>
   );
 };
---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logoimage from "../public/logo-image.webp";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="logo">
+          <img src={logoimage} alt="" title="logo" />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
