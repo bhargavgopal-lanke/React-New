c97192fae7dd01f69b049899ca737d31536da5a3
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-11-19 09:27:13 +0530

    css changes for card

--- components/RestaurentCard.jsx
+++ components/RestaurentCard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import vegBiryani from "../public/veg-biryani.jpeg";
 
 const RestaurentCard = () => {
   return (
     <div className="res-card">
+      <img src={vegBiryani} className="food-img" />
       <h3>Meghana Foods</h3>
+      <h4>Biryai, North Indian, Asian</h4>
+      <div className="card-btm-sec">
+        <h4>4.4</h4>
+        <h4>38mins</h4>
+      </div>
     </div>
   );
 };
