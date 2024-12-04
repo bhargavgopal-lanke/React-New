c445f44ce5cd56227014e3044b359eab3f9c4f33
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-05 01:47:16 +0530

    routing added for all links

---
+++ src/components/HeaderSubRoutes/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header";
+
+const Cart = () => {
+  return (
+    <div>
+      <Header />
+      <div className="body-sec">Cart Page</div>
+    </div>
+  );
+};
+
+export default Cart;
