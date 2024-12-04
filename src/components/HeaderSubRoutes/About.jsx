f488390bd9a23b566d3a1e6dd94b1b79dfec1028
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-05 01:42:26 +0530

    About new component created.

---
+++ src/components/HeaderSubRoutes/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header";
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <div className="body-sec">About Page</div>
+    </div>
+  );
+};
+
+export default About;
