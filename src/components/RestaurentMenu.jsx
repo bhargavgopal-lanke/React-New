e320718421315bc91b6ca3d71f18aee7303369c0
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-07 00:33:43 +0530

    menu card created.

---
+++ src/components/RestaurentMenu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RestaurentMenu = () => {
+  return (
+    <div className="menu">
+      <h1>Name of the Restaurant</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Biryani</li>
+        <li>Burgers</li>
+        <li>Diet Coke</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurentMenu;
