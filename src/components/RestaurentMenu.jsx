c991a357ced20dbe29cdc7bdefc80f3fac56068f
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-07 01:59:05 +0530

    Restaurent menu Api and dynamic logics updated.

--- src/components/RestaurentMenu.jsx
+++ src/components/RestaurentMenu.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MENUAPI } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const RestaurentMenu = () => {
+  const [menuData, setMenuData] = useState([]);
+
+  // if I dont add the dependency array useffect will be called everytime a component renders.
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const menuApiData = await fetch(MENUAPI);
+    const jsonData = await menuApiData.json();
+    setMenuData(jsonData.data);
+  };
+
+  const restaurantMenu =
+    menuData?.cards &&
+    menuData?.cards.length > 2 &&
+    menuData?.cards[2]?.card?.card?.info;
+
+  const { name, cuisines, costForTwoMessage } = restaurantMenu || "";
+
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
-      <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
-      </ul>
+      {menuData === null && <Shimmer />}
+      <>
+        <div>
+          <h1>{name}</h1>
+          <h2>{cuisines.join(", ")}</h2>
+          <h3>{costForTwoMessage}</h3>
+        </div>
+      </>
     </div>
   );
 };
