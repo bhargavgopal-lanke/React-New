06eaa9a62576133efb85c70bee16f0652d9f2888
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-09 15:16:32 +0530

    Api calling using class components.

--- src/components/HeaderSubRoutes/AboutClass.jsx
+++ src/components/HeaderSubRoutes/AboutClass.jsx
@@ -1,49 +1,43 @@
 import { Component } from "react";
+import ContactComponent from "./ContactClass";
 
 class UseClass extends Component {
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super();
     this.state = {
-      count: 0,
-      count2: 2,
+      apiData: [],
     };
   }
 
-  render() {
-    const { name, location } = this.props;
-    const { count, count2 } = this.state;
-    return (
-      <div>
-        <h1>{name}</h1>
-        <h1>{location}</h1>
+  async componentDidMount() {
+    const fetchGitApiData = await fetch(
+      "https://api.github.com/users/akshayMarch7"
+    );
+    const json = await fetchGitApiData.json();
 
-        <div>
-          <h1>Count = {count}</h1>
-          <h1>Count 2 = {count2}</h1>
-        </div>
-        <button
-          type="button"
-          onClick={() =>
-            // Never update state variable directlty
-            this.setState({
-              count: count + 1,
-            })
-          }
-        >
-          onclick One
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            this.setState({
-              count2: count2 + 1,
-            })
-          }
-        >
-          onclick Two
-        </button>
-      </div>
+    this.setState({ apiData: json });
+  }
+
+  render() {
+    const { apiData } = this.state;
+    console.log("key :", Object.entries(apiData));
+    return (
+      <>
+        <h1 style={{marginBottom: "25px"}}>This is an about page.</h1>
+
+        {Object.entries(apiData).map((data, index) => {
+          return (
+            <div>
+              <h1 key={index} style={{marginBottom: "15px"}}>{data[0]}:</h1>
+              <p style={{marginBottom: "25px"}}>{data[1]}</p>
+            </div>
+          );
+        })}
+
+        {/* <h1>{apiData.bio}</h1>
+        <h1>{apiData.location}</h1> */}
+        {/* <ContactComponent name={"Bhargav"} location={"Hyderabad"} /> */}
+      </>
     );
   }
 }
--- src/components/HeaderSubRoutes/ContactClass.jsx
+++ src/components/HeaderSubRoutes/ContactClass.jsx
@@ -1,17 +1,55 @@
 import { Component } from "react";
-import UseClass from "./AboutClass";
 
 class ContactComponent extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    console.log("child constructor");
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+  }
+  componentDidMount() {
+    console.log("child component Did Mount");
   }
 
   render() {
+    console.log("child render");
+    const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
-      <>
-        <h1>This is an about page.</h1>
-        <UseClass name={"Bhargav"} location={"Hyderabad"} />
-      </>
+      <div>
+        <h1>{name}</h1>
+        <h1>{location}</h1>
+
+        <div>
+          <h1>Count = {count}</h1>
+          <h1>Count 2 = {count2}</h1>
+        </div>
+        <button
+          type="button"
+          onClick={() =>
+            // Never update state variable directlty
+            this.setState({
+              count: count + 1,
+            })
+          }
+        >
+          onclick One
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            this.setState({
+              count2: count2 + 1,
+            })
+          }
+        >
+          onclick Two
+        </button>
+      </div>
     );
   }
 }
