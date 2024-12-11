89116002dddc05871d742eb7c2093b659814bf0c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-12 01:44:33 +0530

    using HOC check the promoted data from the API and if exits update the data with new component using HOC.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import RestaurentCard from "./RestaurentCard";
+import RestaurentCard, { withPromotedLabel } from "./RestaurentCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router";
 import useOnlineStatus from "../Helper/useOnlineStatus";
@@ -9,6 +9,8 @@ const BodyRes = () => {
   const [ListOfRatedRes, setListOfRatedRes] = useState([]);
   const [filteredRestaurents, setFilteredRestaurents] = useState([]);
   const [searchInputText, setSearchInputText] = useState("");
+
+  const RestoCardPromoted = withPromotedLabel(RestaurentCard);
 
   useEffect(() => {
     restuarentCardsData();
@@ -30,10 +32,9 @@ const BodyRes = () => {
         .toLowerCase()
         .includes(searchInputText.toLowerCase());
     });
-  
+
     setFilteredRestaurents(filteredRestaurentsList);
   };
-  
 
   const onlineStatus = useOnlineStatus();
 
@@ -71,7 +72,7 @@ const BodyRes = () => {
       </div>
       {/* rendering on the basis of a condition is known as conditional rendering */}
       {ListOfRatedRes.length === 0 && <Shimmer />}
-      <div className="flex justify-between flex-wrap">
+      <div className="res-container">
         {filteredRestaurents.map((data, index) => {
           // v3 api data dstructuring
           //  let resDataList = data?.card?.card?.info;
@@ -80,9 +81,13 @@ const BodyRes = () => {
             <Link
               key={index}
               to={`/restuarent/${data.info.id}`}
-              className="res-card-link flex-1"
+              className="res-card-link"
             >
-              <RestaurentCard key={data.info.id} resData={data.info} />
+              {data.info.promoted === true ? (
+                <RestoCardPromoted key={data.info.id} resData={data.info} />
+              ) : (
+                <RestaurentCard key={data.info.id} resData={data.info} />
+              )}
             </Link>
           );
         })}
