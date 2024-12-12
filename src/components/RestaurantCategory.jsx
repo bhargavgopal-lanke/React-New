7d2cf61ed22ebccb6a7fe7b2e3e35c27e3ab56fd
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-13 00:27:15 +0530

    accordian Ui updated.

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -5,32 +5,11 @@ const RestaurantCategory = ({ categoryList }) => {
 
   return (
     <div className="w-6/12 mx-auto mb-10 bg-gray-50 py-2 px-3 shadow-lg flex justify-between items-center">
-      <span className="font-bold my-3 text-xl">{title} </span>
+      <span className="font-bold my-3 text-xl">
+        {title} ({itemCards.length})
+      </span>
       <span>⬇️</span>
-      {/* <ul className="">
-        {itemCards.map((items) => {
-          const {
-            id,
-            name,
-            price,
-            defaultPrice,
-            category,
-            description,
-          } = items?.card?.info;
-
-          return (
-            <>
-              <li key={id} className="text-lg name">
-                {name}
-              </li>
-              <li className="text-lg">
-                <strong>Rs -</strong> {(price || defaultPrice) / 100}
-              </li>
-              <li className="text-lg"> {description}</li>
-            </>
-          );
-        })}
-      </ul> */}
+    
     </div>
   );
 };
