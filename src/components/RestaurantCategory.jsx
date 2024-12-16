dbcabd1f4b0adb44676713cb95239d6a3cac9a9b
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-17 00:16:53 +0530

    accordian icons updated with functionality

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import CategoryListItems from "./CategoryListItems";
 
 const RestaurantCategory = ({ categoryList }) => {
-  const [categoryDisplay, setCategoryDisplay] = useState(true);
+  const [categoryDisplay, setCategoryDisplay] = useState(false);
   const { title, itemCards } = categoryList;
 
   return (
     <>
       <div className="w-6/12 mx-auto mb-10 bg-gray-50 py-2 px-5 shadow-lg">
-        <div className="flex justify-between items-center">
+        <div
+          className="flex justify-between items-center cursor-pointer"
+          onClick={() => setCategoryDisplay(!categoryDisplay)}
+        >
           <span className="font-bold my-3 text-xl">
             {title} ({itemCards.length})
           </span>
-          <span
-            className="cursor-pointer"
-            onClick={() => setCategoryDisplay(!categoryDisplay)}
-          >
-            ⬇️
-          </span>
+          <span>{!categoryDisplay ? "⬇️" : "⬆️"}</span>
         </div>
         <div>
           {categoryDisplay ? <CategoryListItems data={itemCards} /> : ""}
