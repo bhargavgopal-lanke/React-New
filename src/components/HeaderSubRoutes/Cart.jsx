d3e8bfad3b94bfbf4ecbd4285d10640af1cd0fda
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-12-27 02:47:59 +0530

    empty cart added.

--- src/components/HeaderSubRoutes/Cart.jsx
+++ src/components/HeaderSubRoutes/Cart.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import store from "../../store";
+import { clearCart } from "../../Slices/CartItems-Slice";
 
 const Cart = () => {
-  
   const cartItems = useSelector((state) => state?.cartItemsInfo?.cartItems);
+
+  const handleEmptyCart = () => {
+    store.dispatch(clearCart());
+  };
 
   return (
     <div className="body-sec">
       <div className="mb-3 text-2xl font-bold">Cart Page</div>
+
+      <button type="button" className="" onClick={handleEmptyCart}>
+        Empty Cart
+      </button>
+
       <div
         className="block p-6 max-w-sm
             bg-white-600 rounded-lg 
