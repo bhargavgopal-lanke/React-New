ae1838a5d9c5aff723f64cee4b5cf4044a827275
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-16 17:01:24 -0400

    Wrote two test cases that are working 😊

--- src/components/__tests__/Contact.test.jsx
+++ src/components/__tests__/Contact.test.jsx
@@ -1,12 +1,22 @@
-
-import React from 'react'
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import Contact from "../HeaderSubRoutes/Contact";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
-test("check contact page text", () => {
-  render(<Contact />);
-  const heading = screen.getByRole("heading");
-  // Assertion
-  expect(heading).toBeInTheDocument();
+describe("Contact component testing", () => {
+  beforeEach(() => {
+    return render(<Contact />);
+  });
+
+  test("Should render contact component", () => {
+    const heading = screen.getByRole("heading");
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Should load button inside contact component", () => {
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  
 });
